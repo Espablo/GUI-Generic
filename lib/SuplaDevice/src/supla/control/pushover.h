77056e3d57d6c0d53bf99913d948fa22abf82a0e
krycha88 <krystian.franieczek@gmail.com>
2021-02-05 16:41:35 +0100

    aktualizacja biblioteki pushover

---
+++ lib/SuplaDevice/src/supla/control/pushover.h
@@ -0,0 +1,80 @@
+/*
+ Copyright (C) AC SOFTWARE SP. Z O.O.
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
+#ifndef _pushover_h
+#define _pushover_h
+
+#include <Arduino.h>
+#include <WiFiClientSecure.h>
+
+#include "../action_handler.h"
+#include "../actions.h"
+#include "../channel_element.h"
+
+#define MAX_TOKEN_SIZE   32
+#define MAX_USER_SIZE    32
+#define MAX_TITLE_SIZE   32
+#define MAX_MESSAGE_SIZE 64
+
+namespace Supla {
+namespace Control {
+class Pushover : public Element, public ActionHandler {
+ public:
+  Pushover(const char *token = nullptr,
+           const char *user = nullptr,
+           bool isSecured = true);
+  ~Pushover();
+
+  void setToken(const char *token);
+  void setUser(const char *user);
+  void setMessage(const char *message);
+  void setTitle(const char *title);
+
+  void iterateAlways();
+  void handleAction(int event, int action);
+
+  bool checkConnection();
+  void toggleConnection();
+  bool openConnection();
+  bool closeConnection();
+  void sendRequest();
+  void send();
+
+ protected:
+  WiFiClient *client = NULL;
+  const char *host = "api.pushover.net";
+  const char *path = "/1/messages.json";
+  unsigned long lastMsgReceivedMs;
+
+  bool _isSecured;
+  char _token[MAX_TOKEN_SIZE];
+  char _user[MAX_USER_SIZE];
+  char _title[MAX_TITLE_SIZE];
+  char _message[MAX_MESSAGE_SIZE];
+  String _device;
+  String _url;
+  String _url_title;
+  int8_t _priority = 0;
+  uint16_t _retry;
+  uint16_t _expire;
+  uint32_t _timestamp = 0;
+  String _sound;
+};
+
+};  // namespace Control
+};  // namespace Supla
+
+#endif
