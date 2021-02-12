6123de5d30b7b8c6ec2d6fd81a7b64acec7afc94
krycha88 <krystian.franieczek@gmail.com>
2021-02-12 20:18:24 +0100

    SPIFFS -> LittleFS

--- src/SuplaConfigManager.cpp
+++ src/SuplaConfigManager.cpp
@@ -21,7 +21,7 @@
 #include <IPAddress.h>
 #include <supla-common/proto.h>
 
-#include "FS.h"
+#include "LittleFS.h"
 #include "SuplaConfigManager.h"
 #include "SuplaDeviceGUI.h"
 
@@ -138,10 +138,10 @@ void ConfigOption::setValue(const char *value) {
 // class SuplaConfigManager
 //
 SuplaConfigManager::SuplaConfigManager() {
-  if (SPIFFS.begin()) {
-    //    Serial.println(F("\nSPIFFS mounted"));
+  if (LittleFS.begin()) {
+    //    Serial.println(F("\nLittleFS mounted"));
     //  } else {
-    //    Serial.println(F("\nFailed to mount SPIFFS"));
+    //    Serial.println(F("\nFailed to mount LittleFS"));
   }
   _optionCount = 0;
 
@@ -248,9 +248,9 @@ uint8_t SuplaConfigManager::deleteKey(uint8_t key) {
 }
 
 uint8_t SuplaConfigManager::load() {
-  if (SPIFFS.begin()) {
-    if (SPIFFS.exists(CONFIG_FILE_PATH)) {
-      File configFile = SPIFFS.open(CONFIG_FILE_PATH, "r");
+  if (LittleFS.begin()) {
+    if (LittleFS.exists(CONFIG_FILE_PATH)) {
+      File configFile = LittleFS.open(CONFIG_FILE_PATH, "r");
 
       if (configFile) {
         int i = 0;
@@ -293,9 +293,9 @@ uint8_t SuplaConfigManager::load() {
 }
 
 uint8_t SuplaConfigManager::loadItem(uint8_t key) {
-  if (SPIFFS.begin()) {
-    if (SPIFFS.exists(CONFIG_FILE_PATH)) {
-      File configFile = SPIFFS.open(CONFIG_FILE_PATH, "r");
+  if (LittleFS.begin()) {
+    if (LittleFS.exists(CONFIG_FILE_PATH)) {
+      File configFile = LittleFS.open(CONFIG_FILE_PATH, "r");
 
       if (configFile) {
         int i = 0;
@@ -336,7 +336,7 @@ uint8_t SuplaConfigManager::loadItem(uint8_t key) {
 }
 
 uint8_t SuplaConfigManager::save() {
-  if (SPIFFS.begin()) {
+  if (LittleFS.begin()) {
     int i = 0;
     int offset = 0;
     int length = 0;
@@ -345,7 +345,7 @@ uint8_t SuplaConfigManager::save() {
       length += _options[i]->getLength();
     }
 
-    File configFile = SPIFFS.open(CONFIG_FILE_PATH, "w+");
+    File configFile = LittleFS.open(CONFIG_FILE_PATH, "w+");
     if (configFile) {
       uint8_t *content = (uint8_t *)malloc(sizeof(uint8_t) * length);
       for (i = 0; i < _optionCount; i++) {
