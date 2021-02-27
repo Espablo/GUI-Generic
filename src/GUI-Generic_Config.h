5a7e3a52574917a76ab930488bf194c5336324a5
krycha88 <krystian.franieczek@gmail.com>
2021-02-27 08:51:24 +0100

    Update GUI-Generic_Config.h

--- src/GUI-Generic_Config.h
+++ src/GUI-Generic_Config.h
@@ -26,6 +26,7 @@
 #define SUPLA_LIMIT_SWITCH
 #define SUPLA_ROLLERSHUTTER
 #define SUPLA_CONFIG
+#define SUPLA_LED
 
 // ##### 1Wire #####
 #define SUPLA_DS18B20
@@ -38,7 +39,7 @@
 #define SUPLA_SHT3x
 #define SUPLA_SI7021
 #define SUPLA_OLED
-// #define SUPLA_MCP23017
+#define SUPLA_MCP23017
 // #define SUPLA_HTU21D    // 0x40 NOT SUPPORTED
 // #define SUPLA_SHT71     // 0x44 AND 0x45 NOT SUPPORTED
 // #define SUPLA_BH1750    // 0x23 AND 0x5C NOT SUPPORTED
