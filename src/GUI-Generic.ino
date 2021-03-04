b58fdf00f775373d536f8803ae8c787cf872ef13
krycha88 <krystian.franieczek@gmail.com>
2021-03-04 06:53:35 +0100

    ifdef SUPLA_MCP23017

--- src/GUI-Generic.ino
+++ src/GUI-Generic.ino
@@ -15,7 +15,9 @@
 */
 #include "SuplaDeviceGUI.h"
 
+#ifdef SUPLA_MCP23017
 #include <supla/control/MCP23017/supla_mcp23017.h>
+#endif
 
 #define DRD_TIMEOUT 5  // Number of seconds after reset during which a subseqent reset will be considered a double reset.
 #define DRD_ADDRESS 0  // RTC Memory Address for the DoubleResetDetector to use
