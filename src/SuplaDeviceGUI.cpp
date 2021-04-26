bf1e7bf25ee7537e7ab1aa35c13226e6d8c6d16c
krycha88 <krystian.franieczek@gmail.com>
2021-04-26 08:04:24 +0200

    poprawa kompilacja dla CSE7766

--- src/SuplaDeviceGUI.cpp
+++ src/SuplaDeviceGUI.cpp
@@ -17,7 +17,7 @@
 #include "SuplaConfigManager.h"
 #include <supla/network/SuplaGuiWiFi.h>
 
-#if defined(SUPLA_RELAY) || defined(SUPLA_ROLLERSHUTTER) || defined(SUPLA_IMPULSE_COUNTER) || defined(SUPLA_HLW8012)
+#if defined(SUPLA_RELAY) || defined(SUPLA_ROLLERSHUTTER) || defined(SUPLA_IMPULSE_COUNTER) || defined(SUPLA_HLW8012) || defined(SUPLA_CSE7766)
 #define TIME_SAVE_PERIOD_SEK                 30   // the time is given in seconds
 #define TIME_SAVE_PERIOD_IMPULSE_COUNTER_SEK 600  // 10min
 #define STORAGE_OFFSET                       0
