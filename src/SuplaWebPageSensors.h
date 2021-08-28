c7021d0fb77149835b74d611f35d0a40e33b524a
krycha88 <krystian.franieczek@gmail.com>
2021-08-28 19:49:02 +0200

    Poprawki definów dla sensorów

--- src/SuplaWebPageSensors.h
+++ src/SuplaWebPageSensors.h
@@ -1,17 +1,12 @@
 #ifndef SuplaWebPageSensors_h
 #define SuplaWebPageSensors_h
 
-enum _sensorI2C
-{
-  SENSOR_I2C_BME280,
-  SENSOR_I2C_SHT3x,
-  SENSOR_I2C_SI7021,
-  SENSOR_I2C_MAX6675,
-  SENSOR_I2C_OLED,
-  SENSOR_I2C_MCP23017,
-  SENSOR_I2C_BMP280,
-  SENSOR_I2C_VL53L0X
-};
+#include "GUI-Generic_Config.h"
+#include "SuplaWebPageSensor1Wire.h"
+#include "SuplaWebPageSensorI2c.h"
+#include "SuplaWebPageSensorSpi.h"
+#include "SuplaWebPageSensorAnalog.h"
+#include "SuplaWebPageOther.h"
 
 void createWebPageSensors();
 
