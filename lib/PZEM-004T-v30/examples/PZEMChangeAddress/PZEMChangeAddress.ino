17e27729d504420e41432cd945357f3f9366ade8
krycha88 <krystian.franieczek@gmail.com>
2021-08-29 12:34:39 +0200

    aktualizacja biblioteki PZEMv3

--- lib/PZEM-004T-v30/examples/PZEMChangeAddress/PZEMChangeAddress.ino
+++ lib/PZEM-004T-v30/examples/PZEMChangeAddress/PZEMChangeAddress.ino
@@ -1,21 +1,117 @@
+/*
+Copyright (c) 2021 Jakub Mandula
+
+Example of setting a custom ModBUS address for the PZEM modules.
+================================================================
+
+Each PZEM modules has two addresses. A default general address (0xF8),
+which every module will listen on by default. And a custom address.
+
+The custom address can be used if multiple PZEM modules are to be used
+on a single ModBUS.
+
+This script will use the general address in order to set the custom 
+address of the connected PZEM module.
+
+Therefore make sure only one PZEM module is connected at a time! 
+Otherwise all connected modules will receive the same custom address.
+
+*/
+
 #include <PZEM004Tv30.h>
 
-PZEM004Tv30 pzem(&Serial3);
+
+#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
+#define PZEM_RX_PIN 16
+#define PZEM_TX_PIN 17
+#endif
+
+#if !defined(PZEM_SERIAL)
+#define PZEM_SERIAL Serial2
+#endif
+
+/* Hardware Serial2 is only available on certain boards.
+ * For example the Arduino MEGA 2560
+*/
+#if defined(USE_SOFTWARE_SERIAL)
+#include <SoftwareSerial.h>
+/*************************
+ *  Use SoftwareSerial for communication
+ * ---------------------
+ * 
+ * The ESP32 platform does not support the SoftwareSerial as of now 
+ * Here we initialize the PZEM on SoftwareSerial with RX/TX pins PZEM_RX_PIN and PZEM_TX_PIN
+ */
+SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
+PZEM004Tv30 pzem(pzemSWSerial);
+
+#elif defined(ESP32)
+/*************************
+ *  ESP32 initialization
+ * ---------------------
+ * 
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on PZEM_SERIAL with RX/TX pins PZEM_RX_PIN and PZEM_TX_PIN
+ */
+PZEM004Tv30 pzem(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN);
+
+#else
+/*************************
+ *  Arduino/ESP8266 initialization
+ * ---------------------
+ * 
+ * Not all Arduino boards come with multiple HW Serial ports.
+ * Serial2 is for example available on the Arduino MEGA 2560 but not Arduino Uno!
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on PZEM_SERIAL with default pins
+ */
+PZEM004Tv30 pzem(PZEM_SERIAL);
+
+#endif
+
+/*******************************************
+ * Set your address over here..
+ * The address can be between 0x01 and 0xF7
+ *******************************************/
+#if !defined(SET_ADDRESS)
+#define SET_ADDRESS 0x55
+#endif
+
+// Set to true in order to increment the address every iteration
+#define INCREMENT false
+
 
 void setup() {
   Serial.begin(115200);
 }
 
-uint8_t addr = 0x01;
-
 void loop() {
-    pzem.setAddress(addr);
-    Serial.print("Current address:");
-    Serial.println(pzem.getAddress());
-    Serial.println();
+    static uint8_t addr = SET_ADDRESS;
 
-    if(++addr == 0xF8)
+    // Print out current custom address
+    Serial.print("Previous address:   0x");
+    Serial.println(pzem.readAddress(), HEX);
+
+    // Set the custom address
+    Serial.print("Setting address to: 0x");
+    Serial.println(addr, HEX);
+    if(!pzem.setAddress(addr))
+    {
+      // Setting custom address failed. Probably no PZEM connected
+      Serial.println("Error setting address.");
+    } else {
+      // Print out the new custom address
+      Serial.print("Current address:    0x");
+      Serial.println(pzem.readAddress(), HEX);
+      Serial.println();
+    }
+
+    // Increment the address every loop if desired
+    if(INCREMENT){
+      addr++;
+      if(addr >= PZEM_DEFAULT_ADDR)
         addr = 0x01;
+    }
 
-    delay(1000);
+    delay(5000);
 }
--- lib/PZEM-004T-v30/examples/PZEMHardSerial/PZEMHardSerial.ino
+++ lib/PZEM-004T-v30/examples/PZEMHardSerial/PZEMHardSerial.ino
@@ -1,56 +1,109 @@
+/*
+Copyright (c) 2021 Jakub Mandula
+
+Example of using one PZEM module with Hardware Serial interface.
+================================================================
+
+If desired, a HardwareSerial handle can be passed to the constructor
+which will then be used for the communication with the module.
+
+Note that ESP32 HardwareSerial must also be provided with the RX and TX
+pins.
+
+*/
+
 #include <PZEM004Tv30.h>
 
-/* Hardware Serial3 is only available on certain boards.
- * For example the Arduino MEGA 2560
-*/
-PZEM004Tv30 pzem(&Serial3);
+
+#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
+#define PZEM_RX_PIN 16
+#define PZEM_TX_PIN 17
+#endif
+
+#if !defined(PZEM_SERIAL)
+#define PZEM_SERIAL Serial2
+#endif
+
+
+#if defined(ESP32)
+/*************************
+ *  ESP32 initialization
+ * ---------------------
+ * 
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on Serial2 with RX/TX pins 16 and 17
+ */
+PZEM004Tv30 pzem(PZEM_SERIAL, PZEM_RX_PIN, PZEM_TX_PIN);
+#elif defined(ESP8266)
+/*************************
+ *  ESP8266 initialization
+ * ---------------------
+ * 
+ * Not all Arduino boards come with multiple HW Serial ports.
+ * Serial2 is for example available on the Arduino MEGA 2560 but not Arduino Uno!
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on Serial2 with default pins
+ */
+//PZEM004Tv30 pzem(Serial1);
+#else
+/*************************
+ *  Arduino initialization
+ * ---------------------
+ * 
+ * Not all Arduino boards come with multiple HW Serial ports.
+ * Serial2 is for example available on the Arduino MEGA 2560 but not Arduino Uno!
+ * The ESP32 HW Serial interface can be routed to any GPIO pin 
+ * Here we initialize the PZEM on Serial2 with default pins
+ */
+PZEM004Tv30 pzem(PZEM_SERIAL);
+#endif
 
 void setup() {
-  Serial.begin(115200);
+    // Debugging Serial port
+    Serial.begin(115200);
+
+    // Uncomment in order to reset the internal energy counter
+    // pzem.resetEnergy()
 }
 
 void loop() {
+    // Print the custom address of the PZEM
+    Serial.print("Custom Address:");
+    Serial.println(pzem.readAddress(), HEX);
+
+    // Read the data from the sensor
     float voltage = pzem.voltage();
-    if(!isnan(voltage)){
-        Serial.print("Voltage: "); Serial.print(voltage); Serial.println("V");
-    } else {
-        Serial.println("Error reading voltage");
-    }
-
     float current = pzem.current();
-    if(!isnan(current)){
-        Serial.print("Current: "); Serial.print(current); Serial.println("A");
-    } else {
-        Serial.println("Error reading current");
-    }
-
     float power = pzem.power();
-    if(!isnan(power)){
-        Serial.print("Power: "); Serial.print(power); Serial.println("W");
-    } else {
-        Serial.println("Error reading power");
-    }
-
     float energy = pzem.energy();
-    if(!isnan(energy)){
-        Serial.print("Energy: "); Serial.print(energy,3); Serial.println("kWh");
-    } else {
-        Serial.println("Error reading energy");
-    }
-
     float frequency = pzem.frequency();
-    if(!isnan(frequency)){
-        Serial.print("Frequency: "); Serial.print(frequency, 1); Serial.println("Hz");
-    } else {
+    float pf = pzem.pf();
+
+    // Check if the data is valid
+    if(isnan(voltage)){
+        Serial.println("Error reading voltage");
+    } else if (isnan(current)) {
+        Serial.println("Error reading current");
+    } else if (isnan(power)) {
+        Serial.println("Error reading power");
+    } else if (isnan(energy)) {
+        Serial.println("Error reading energy");
+    } else if (isnan(frequency)) {
         Serial.println("Error reading frequency");
+    } else if (isnan(pf)) {
+        Serial.println("Error reading power factor");
+    } else {
+
+        // Print the values to the Serial console
+        Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
+        Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
+        Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
+        Serial.print("Energy: ");       Serial.print(energy,3);     Serial.println("kWh");
+        Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
+        Serial.print("PF: ");           Serial.println(pf);
+
     }
 
-    float pf = pzem.pf();
-    if(!isnan(pf)){
-        Serial.print("PF: "); Serial.println(pf);
-    } else {
-        Serial.println("Error reading power factor");
-    }
 
     Serial.println();
     delay(2000);
