c8fdeb7083bcc9c185b90aa50b8efdb5ab6cd955
krycha88 <krystian.franieczek@gmail.com>
2021-08-31 14:04:13 +0200

    Update SuplaConfigESP.cpp

--- src/SuplaConfigESP.cpp
+++ src/SuplaConfigESP.cpp
@@ -14,6 +14,7 @@
   Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 #include "SuplaConfigESP.h"
+#include "SuplaDeviceGUI.h"
 
 SuplaConfigESP::SuplaConfigESP() {
   configModeESP = NORMAL_MODE;
