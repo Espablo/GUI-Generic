e49c4eab9dbae6c2332a51a1779c136b34dc65af
krycha88 <krystian.franieczek@gmail.com>
2022-01-09 16:25:22 +0100

    Zmiana czasu delay(150) przy starcie ESP

--- src/GUI-Generic.ino
+++ src/GUI-Generic.ino
@@ -45,7 +45,7 @@ void setup() {
   for (uint8_t t = 4; t > 0; t--) {
     Serial.printf("[SETUP] WAIT %d...\n", t);
     Serial.flush();
-    delay(250);
+    delay(150);
   }
 
 #ifdef ARDUINO_ARCH_ESP8266
