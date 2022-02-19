ca9fa555004775cfc4309ccb315ae8cba8fffdbc
krycha88 <krystian.franieczek@gmail.com>
2022-02-19 08:02:52 +0100

    Usunięcie restartu urządzenia przy wywołaniu handleNotFound

--- src/SuplaWebServer.cpp
+++ src/SuplaWebServer.cpp
@@ -144,7 +144,6 @@ void SuplaWebServer::sendContent() {
 void SuplaWebServer::handleNotFound() {
   httpServer->sendHeader("Location", PATH_START, true);
   handlePageHome(2);
-  ConfigESP->rebootESP();
 }
 
 bool SuplaWebServer::isLoggedIn() {
