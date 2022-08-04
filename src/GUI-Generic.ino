cfee32be3435347515ac62df4348d53500dfce2b
krycha88 <krystian.franieczek@gmail.com>
2022-08-04 10:29:31 +0200

    Procent dla SUPLA_MPX_5XXX

--- src/GUI-Generic.ino
+++ src/GUI-Generic.ino
@@ -589,7 +589,7 @@ void setup() {
     Supla::Sensor::Percentage *mpxPercent;
 
     if (Supla::GUI::mpx->getThankHeight() != 0) {
-      mpxPercent = new Supla::Sensor::Percentage(Supla::GUI::mpx, 0, Supla::GUI::mpx->getThankHeight() * 0.01);
+      mpxPercent = new Supla::Sensor::Percentage(Supla::GUI::mpx, 0, Supla::GUI::mpx->getThankHeight());
     }
     else {
       mpxPercent = new Supla::Sensor::Percentage(Supla::GUI::mpx, 0, 100);
