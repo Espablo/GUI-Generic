eeba3b578918407ed87a61f4f6666f18a28fadf4
krycha88 <krystian.franieczek@gmail.com>
2022-08-05 10:02:29 +0200

    poprawa ścieżki dostępu dla pin_status_led.h

--- src/SuplaDeviceGUI.h
+++ src/SuplaDeviceGUI.h
@@ -114,7 +114,7 @@
 #ifdef SUPLA_CSE7766
 #include <supla/sensor/CSE_7766.h>
 #endif
-#include <src/control/pin_status_led.h>
+#include "src/control/pin_status_led.h"
 
 #ifdef SUPLA_RGBW
 #include <supla/control/rgbw_leds.h>
