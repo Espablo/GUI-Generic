1ea9e6a1d309eb5066ba93fc63ce613c7993549a
krycha88 <krystian.franieczek@gmail.com>
2022-08-25 20:28:12 +0200

    pushover - wyświetlanie odpowiedź z serwera

--- lib/SuplaDeviceExtensions/src/supla/control/pushover.cpp
+++ lib/SuplaDeviceExtensions/src/supla/control/pushover.cpp
@@ -157,9 +157,9 @@ void Pushover::toggleConnection() {
       client = new WiFiClientSecure();
       ((WiFiClientSecure *)client)->setInsecure();
 #ifdef ARDUINO_ARCH_ESP8266
-      ((WiFiClientSecure *)client)->setBufferSizes(256, 256);
+      ((WiFiClientSecure *)client)->setBufferSizes(1024, 512);
 #endif
-      ((WiFiClientSecure *)client)->setTimeout(200);
+      ((WiFiClientSecure *)client)->setTimeout(1000);
     } else {
       client = new WiFiClient();
     }
@@ -185,25 +185,24 @@ void Pushover::sendRequest() {
 
     client->print(String("POST ") + path + " HTTP/1.1\r\n" + "host: " + host +
                   "\r\n" + "Content-length: " + String(post.length(), DEC) +
-                  "\r\n"
+                  "\r\n" +
                   "Content-Type: application/x-www-form-urlencoded\r\n" +
-                  "Connection: close\r\n\r\n" + post);
+                  "Cache-Control: no-cache\r\n\r\n" + post);
 
-    while (client->connected()) {
-      String line = client->readStringUntil('\n');
-      if (line == "\r") {
+    while (client->connected() || client->available()) {
+      if (client->readStringUntil('\n') == "\r") {
         Serial.println(F("Pushover - Headers received"));
         break;
       }
     }
 
-    /* String line = client->readString();
-     if (line.indexOf("\"status\":1") != -1 || line.indexOf("200 OK") != -1) {
-       Serial.println(F("Alert sent successfully!"));
-     } else {
-       Serial.print(F("Alert failure"));
-       Serial.println(line);
-     }*/
+    String line = client->readString();
+    if (line.indexOf("\"status\":1") != -1 || line.indexOf("200 OK") != -1) {
+      Serial.println(F("Alert sent successfully!"));
+    } else {
+      Serial.println(F("Alert failure"));
+      Serial.println(line);
+    }
   }
 }
 
