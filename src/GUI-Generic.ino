edc59338e35f7f9fbf6240abb432b7a67a3273e5
krycha88 <krystian.franieczek@gmail.com>
2022-10-30 19:10:58 +0100

    wymuszenie force400khz dla PCF

--- src/GUI-Generic.ino
+++ src/GUI-Generic.ino
@@ -638,12 +638,14 @@ void setup() {
 #ifdef SUPLA_PCF8574
     if (ConfigManager->get(KEY_ACTIVE_SENSOR)->getElement(SENSOR_I2C_PCF857X).toInt()) {
       new Supla::Control::PCF_8574();
+      force400khz = true;
     }
 #endif
 
 #ifdef SUPLA_PCF8575
     if (ConfigManager->get(KEY_ACTIVE_SENSOR)->getElement(SENSOR_I2C_PCF857X).toInt()) {
       new Supla::Control::PCF_8575();
+      force400khz = true;
     }
 #endif
 
